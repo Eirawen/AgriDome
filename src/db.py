5510932bcfce0af011721fd6b28d80947cb628fb
Eirawen <kmessai@stanford.edu>
2024-05-18 00:10:10 -0700

    First Commit

---
+++ src/db.py
@@ -0,0 +1,6 @@
+import os
+import csv
+import sqlite3
+import random 
+from dataclasses import dataclass
+import datetime
---
+++ src/scraper.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import re
+import utils
+import selenium
+from selenium import webdriver
